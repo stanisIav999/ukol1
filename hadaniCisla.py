023c40ab5ecdf2b9dfc0e0d17281122140bae310
stanisIav999 <popovych.s@ssmg.cz>
2026-02-05 08:54:31 +0100

    random

--- hadaniCisla.py
+++ hadaniCisla.py
@@ -1,2 +1,14 @@
-import random
-print(random.random())
+import  random
+nah=random.randint(1,100)
+pocet=0
+while True:
+    cs = int(input("Zadej cislo:"))
+    pocet=pocet+1
+    if cs == nah:
+        break
+    elif cs < nah:
+        print("won")
+
+    else:
+        print("lose")
+print("uhodnuto na",pocet,"pokusu")
